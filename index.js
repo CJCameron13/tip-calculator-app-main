ee2b1e859d9c91da2360f9602419e97f33d81720
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2024-09-23 14:39:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const totalBill = document.querySelector('#bill')
 const partyNumber = document.querySelector('#number-of-people')
-const fivePercent = document.querySelector("#5%")
-const tenPercent = document.querySelector('#10%')
-const fifteenPercent = document.querySelector('#15%')
-const twentyPercent = document.querySelector('#20%')
-const fiftyPercent = document.querySelector('#50%')
+const fivePercent = document.querySelector("#five-percent")
+const tenPercent = document.querySelector('#ten-percent')
+const fifteenPercent = document.querySelector('#fifteen-percent')
+const twentyFivePercent = document.querySelector('#twenty-five-percent')
+const fiftyPercent = document.querySelector('#fifty-percent')
+const percentageButton = document.querySelectorAll('.percentage-button')
+
+const tipPerPerson = document.querySelector('#tip-per-person')
+
+
+percentageButton.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        console.log(event.target)
+        console.log('targeted')
+    })
+})
